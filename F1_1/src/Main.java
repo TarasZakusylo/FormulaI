b1b1575a3ffea2d80a7d8669b9d52346e399db0d
Taras Zakusylo <tapac.zakusylo@gmail.com>
2016-10-28 17:45:36 +0200

    перевірка на зміну

--- F1_1/src/Main.java
+++ F1_1/src/Main.java
@@ -4,11 +4,11 @@ import javax.swing.JFrame;
 public class Main {
 	public static void main (String [] args){
 		
-		JFrame f = new JFrame("Java F1");
+		JFrame f = new JFrame("Java F 1");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(1100,630);	
 		f.add(new Road());
-		f.setVisible(true);
+		f.setVisible(true);	
 	    f.setResizable(false);
 	    f.setLocationRelativeTo(null);
 	}
